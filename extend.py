bffbd14ce84f8db062f98bb1309c3be93897fa13
xloem <0xloem@gmail.com>
2021-08-16 04:31:51 +0000

    add simple cmdline parameters to extend.py

--- extend.py
+++ extend.py
@@ -1,29 +1,45 @@
 #!/usr/bin/env python3
 
+import argparse
 import sys
-sys.stderr.write('Loading ...\n')
-import generate
-genji = generate.genji()
+import inspect
 
-if len(sys.argv) <= 1:
+import generate
+
+parser = argparse.ArgumentParser(description='extend code or text')
+parser.add_argument('--model', default='genji', help=','.join(generate.MODELS.keys()))
+parser.add_argument('--apikey', default=None, help='needed for openai or ai21')
+parser.add_argument('files', default=[sys.stdin], nargs='*', type=argparse.FileType('a+'), help='files to mutate if not stdio')
+params = parser.parse_args()
+
+sys.stderr.write('Loading ...\n')
+model = generate.MODELS[params.model]
+modparams = dict()
+if params.apikey is not None and 'apikey' in inspect.signature(model).parameters:
+    modparams['apikey'] = params.apikey
+model = model(**modparams)
+
+if params.files[0] is sys.stdin:
     sys.stderr.write('Reading ...\n')
-    data = sys.stdin.read()
+    data = params.files[0].read()
     sys.stderr.write('Generating ...\n')
     # could have it start outputing during pauses
-    result = genji(data)
-    print(result[0]['generated_text'])
+    sys.stdout.write(data)
+    result = model(data, return_full_text=False)
+    sys.stdout.write(result[0]['generated_text'])
 else:
     while True:
         sys.stderr.write('Reading ...\n')
         data = []
-        for fn in sys.argv[1:]:
-            with open(fn, 'rt') as f:
-                data.append(f.read())
+        for f in params.files:
+            f.seek(0)
+            data.append(f.read())
         sys.stderr.write('Generating ...\n')
-        results = genji(data)
-        for fn, result in zip(sys.argv[1:], results):
-            with open(fn, 'at') as f:
-                f.write(result['generated_text'])
+        results = model(data, return_full_text=False)
+        if len(data) == 1:
+            results = [results]
+        for f, result in zip(params.files, results):
+            f.write(result[0]['generated_text'])
         print('Files appended to.  Will reprocess files if a linebreak is received on stdin.')
         if not sys.stdin.readline():
             break

05415f5e45e0d8eacf229b0f6838934fdf9b050b
0xloem <0xloem@gmail.com>
2021-09-29 23:15:32 -0400

    fudged around for some time, and this is the current state.  after understanding gpt models a smidge better, hardness has been reverted some and  property prompt tokens are being generated but they still do not repeat elsewhere

--- codesynth/softprompt.py
+++ codesynth/softprompt.py
@@ -1,7 +1,10 @@
 import torch
 
-# we can try just making up how training works, and see if anything happens ;p
 
+# we can try just making up how training works, and see if anything happens ;p
+            
+
+# something like this was used in the example code referenced by the paper
 class ConstantWarmupLR(torch.optim.lr_scheduler._LRScheduler):
     '''Applies a constant warmup learning rate.  Should be last in chain to override other learning rates.'''
     def __init__(self, optimizer, epochs = 1, lr = 1e-5):
@@ -13,7 +16,6 @@ class ConstantWarmupLR(torch.optim.lr_scheduler._LRScheduler):
             return [self.lr for base_lr in self.base_lrs]
         else:
             return self.base_lrs
-            
 
 # can get embeddings from tokens with model.wte(input_ids)
 class SoftPromptTrainable:
@@ -22,9 +24,11 @@ class SoftPromptTrainable:
         model,
         num_embeds = 128,
         optimizer = torch.optim.SGD,
-        optimizer_params = dict(lr=0.002),
+        optimizer_params = dict(lr=0.02),#0.002),
         hardness = 0, # weighting for nearness to real token ids, can be changed later
         hardness_lite = True, # only measures gradients for each embed to one vocab word
+        #hardness_cpu = True, # calculates distance to all vocab words on cpu, saves vram, but doesn't make use of gpu
+        straight_logits_loss = False,
         lr_schedulers = [
             (torch.optim.lr_scheduler.CosineAnnealingWarmRestarts, dict(T_0=50)),
             (ConstantWarmupLR, dict()),
@@ -33,9 +37,12 @@ class SoftPromptTrainable:
         self.model = model
         self.hardness = hardness
         self.hardness_lite = hardness_lite
+        #self.hardness_cpu = hardness_cpu
+        self.straight_logits_loss = straight_logits_loss
         if hasattr(self.model, 'config'):
             self.config = self.model.config
         self._training = None
+        self._etw = None
 
         self._optimizer_class = optimizer
         self._optimizer_params = optimizer_params
@@ -96,6 +103,14 @@ class SoftPromptTrainable:
         for sched in self.scheds:
             sched.step()
 
+    #@property:
+    #def etw(self):
+    #    if self._etw is None:
+    #        # each embedding has a neighborhood size, where a token is more near to it than any other
+    #        # the closest 2 embeddings have the smallest neighborhood size
+    #        self.etw
+                    
+
     def forward_and_backward(self, pad_token_id, requested_outputs, *params, **kwparams):
         # stretch embeddings to include the requested outputs, shifting one token for being input
         embeds = torch.cat([self.param.expand(requested_outputs.shape[0], *self.param.shape), self.wte(requested_outputs[:,:-1])], dim=1)
@@ -110,36 +125,65 @@ class SoftPromptTrainable:
 
         # manual loss
         logits = self.model(*params, inputs_embeds=embeds, labels=None, return_dict=False, **kwparams)[0]
+        #                    batch      token spot   logits
         output_logits = logits[:, self.num_embeds-1:, :].contiguous()
             # todo? a weight could optionally be multiplied in for each requested output
-        loss = torch.nn.functional.cross_entropy(output_logits.view(-1, output_logits.size(-1)), requested_outputs.view(-1), ignore_index = pad_token_id)
+        if not self.straight_logits_loss:
+            loss = torch.nn.functional.cross_entropy(output_logits.view(-1, output_logits.size(-1)), requested_outputs.view(-1), ignore_index = pad_token_id)
+        else:
+            shape = requested_outputs.shape
+            requested_logits = output_logits[torch.arange(shape[0]).repeat(shape[1],1).t(), torch.arange(shape[1]).repeat(shape[0],1), requested_outputs]
+            #requested_probs = torch.sigmoid(requested_logits).prod(dim=-1)
+            loss = -requested_logits.mean()
 
         # hardness provides for embeddinss to be trained to match tokens
+        '''
+            the logits output by the model are not the input tokens.
+            they would be near them only if the input is similar to the model's training set.
+            the fastest way to find them would be to form a graph of nearness (precalculated 128d voronoi diagram)
+        '''
         if self.hardness:
+            embeds = self.embeds if self.hardness_lite else self.param
             vocab = self.wte.weight
+            #if self.hardness_cpu:
+            #    embeds = embeds.cpu()
+            #    vocab = vocab.cpu()
+
+            embed_vocab_distances = []
+            for embed in embeds:
+                distance = embed.expand(self.vocab_size, *embed.shape) - vocab
+                distance = (distance * distance).sum(dim=-1)
+                embed_vocab_distances.append(distance)
+            embed_vocab_distances = torch.stack(embed_vocab_distances).t()
+                    
+            # vector cross difference
+            #embed_vocab_distances = (
+            #    # embeds rows
+            #    embeds.expand(self.vocab_size, *self.param.shape) -
+            #    # vocab cols
+            #    vocab.expand(self.num_embeds, *vocab.shape).permute(1, 0, 2)
+            #)
+            # self-dot makes squared distance
+            #embed_vocab_distances = (embed_vocab_distances * embed_vocab_distances).sum(dim=-1)
+
+            # consider only the distance of each embed to the nearest vocab word
+            nearest_vocab_words = embed_vocab_distances.min(dim=0)
+                    # NEAREST TOKENS ARE .indices HERE
 
             if self.hardness_lite:
-                with torch.no_grad():
-                    first_token_id = torch.argmax((vocab * self.embeds[0].expand(vocab.shape[0], self.embed_dim)).sum(dim=-1))
-                    token_ids = torch.cat((torch.tensor((first_token_id,), device=self.model.device), torch.argmax(logits[0][:self.num_embeds-1], dim=-1)))
-                    token_embeds = self.wte(token_ids)
-                embed_vocab_distances = token_embeds - self.param
+                embed_vocab_distances = self.wte.weight[nearest_vocab_words.indices] - self.param
                 embed_vocab_distances = (embed_vocab_distances * embed_vocab_distances).sum(dim=-1)
             else:
-                # vector cross difference
-                embed_vocab_distances = (
-                    # embeds rows
-                    self.param.expand(self.vocab_size, *self.param.shape) -
-                    # vocab cols
-                    vocab.expand(self.num_embeds, *vocab.shape).permute(1, 0, 2)
-                )
-                # self-dot makes squared distance
-                embed_vocab_distances = (embed_vocab_distances * embed_vocab_distances).sum(dim=-1)
-
-                # consider only the distance of each embed to the nearest vocab word
-                embed_vocab_distances = embed_vocab_distances.min(dim=0).values
-
+                embed_vocab_distances = nearest_vocab_words.values
             loss += self.hardness * embed_vocab_distances.mean()
+
+#asdfasdf
+#            token_ids = torch.cat((torch.tensor((first_token_id,), device=self.model.device), torch.argmax(logits[0][:self.num_embeds-1], dim=-1)))
+#            # manual loss
+#            input_ids = torch.cat([token_ids.expand(requested_outputs.shape[0], *token_ids.shape), requested_outputs[:,:-1]], dim=1)
+#            logits = self.model(*params, input_ids=input_ids, labels=None, return_dict=False, **kwparams)[0]
+#            output_logits = logits[:, self.num_embeds-1:, :].contiguous()
+#            loss += self.hardness * torch.nn.functional.cross_entropy(output_logits.view(-1, output_logits.size(-1)), requested_outputs.view(-1), ignore_index = pad_token_id)
 
         loss.backward()
         return loss.detach()
@@ -199,15 +243,15 @@ class SoftPromptTrainable:
         running_embeds = self.embeds.clone().detach()
         idx = 0
 
-        # first yield the closest token to the first embedding, which the model won't output
-        dists = (self.wte.weight * self.embeds[0].expand(self.wte.weight.shape[0], self.embed_dim)).sum(dim=-1)
-        yield torch.argmax(dists)
+        # first yield tokens associated with the embeddings temselves
+        for embed in self.embeds:
+            dists = (self.wte.weight * embed.expand(self.wte.weight.shape[0], self.embed_dim)).sum(dim=-1)
+            yield torch.argmax(dists)
 
         while True:
-            new_tokens = torch.argmax(self.model(inputs_embeds = running_embeds, **kwparams)[0][idx:], dim=-1)
-            yield from new_tokens
-            idx += new_tokens.shape[0]
-            running_embeds = torch.cat([running_embeds, self.wte(new_tokens[-1:])])
+            new_token = torch.argmax(self.model(inputs_embeds = running_embeds, **kwparams)[0][-1:],dim=-1)
+            yield new_token[0]
+            running_embeds = torch.cat([running_embeds, self.wte(new_token)])
     def array_tokens(self, len, **kwparams):
         return [token for token, idx in zip(self.generate_tokens(**kwparams), range(len))]
             
@@ -243,6 +287,8 @@ class SoftPromptTrainable:
 
         self.param = torch.nn.Parameter(self.embeds)
 
+        self.embeds_cells = [None] * num_embeds
+
         # might be able to mutate the parameter list live an dnot recreate the optimizers,
         # don't know
         self.optim = self._optimizer_class([self.param], **self._optimizer_params)
@@ -263,3 +309,122 @@ class SoftPromptTrainable:
             self.num_embeds = len(embeds)
         with torch.no_grad():
             self.embeds[:] = embeds
+
+def nonvoronoi_cell(vec, other_vecs):
+    '''approximates the nd vectors from a list that neighbor a given nd vector in nearness'''
+    orig_vecs = other_vecs
+    other_vecs = other_vecs - vec
+    neighbors = [0]
+    untested = [*zip(range(1,len(other_vecs)),other_vecs[1:])]
+    while len(untested):
+        candidate_idx, candidate = untested.pop()
+        cand_dist_squared = (candidate * candidate).sum()
+        candidate_unobstructed = True
+        for neighbor_idx in neighbors:
+            if neighbor_idx == candidate_idx:
+                continue
+            neighbor = other_vecs[neighbor_idx]
+            neighb_dot_cand = (neighbor * candidate).sum()
+            if neighb_dot_cand <= 0:
+                # vectors are in opposite directions
+                continue
+            neighb_dist_squared = (neighbor * neighbor).sum()
+
+            ratio = (cand_dist_squared - neighb_dist_squared) / (2 * (cand_dist_squared - neighb_dot_cand))
+            # ratio is the squared distance to the candidate at which a point is equidistant to candidate and neighbor
+            # derivation is preserved in this block of comments to help me understand this in the future; typed by hand so likely has typos
+            # below, 't' is ratio and 'vec' is candidate, 'elem' is an element of vec and 'elem2' is an element of neighbor
+            # dot(t * vec - vec) = dot(t * vec - neighbor)
+            # sum((t * elem - elem2)**2)
+            # sum(t**2 * elem**2 - 2t * elem * elem2 + elem2**2)
+            # t**2 * sum(elem**2) - 2t * sum(elem * elem2) + sum(elem2**2)
+            # quadratic equations in t
+            # A = dot(vec, vec)
+            # B = -2 dot(vec, neighbor)
+            # C = dot(neighbor, neighbor)
+            # except with vec=neighbor on one side
+            # so combining them
+            # A = 0
+            # B = -2 dot(vec, vec) + 2 dot(vec, neighbor)
+            # C = dot(vec, vec) - dot(neighbor, neighbor)
+            # and if there's any correctness to that, it's now linear
+            # t = (dot(vec, vec) - dot(neighbor, neighbor)) / (2 * (dot(vec, vec) - dot(vec, neighbor)))
+            # t**2 * sum(elem**2) - 2t * sum(elem * elem2) + sum(elem2**2)
+            if ratio < 0:
+                continue
+            if ratio < 1:
+                candidate_unobstructed = False
+                break
+            if ratio > 1:
+                untested.extend(zip(neighbors, other_vecs[neighbors]))
+                neighbors = []
+                break
+        if candidate_unobstructed:
+            neighbors.append(candidate_idx)
+    return orig_vecs[neighbors]
+            
+# it's not voronoi.  voronoi is hard.
+#def _test_voronoi(manual_seed = 0):
+#    torch.manual_seed(manual_seed)
+#    import scipy.spatial
+#    points = torch.randn(16, 2)
+#    npoints = points.numpy()
+#
+#    # just looking for neighboring points, not sure how to use voronoi/delauny structures
+#    # below makes a dict of neighboring point idxs using scipy
+#    voronoi = scipy.spatial.Voronoi(npoints)
+#
+#    vertices_to_neighboring_points = {}
+#    points_to_neighbors = {}
+#
+#    for point_idx, region_idx in enumerate(voronoi.point_region):
+#        region = voronoi.regions[region_idx]
+#        for vertex_idx in region:
+#            if vertex_idx != -1:
+#                if vertex_idx not in vertices_to_neighboring_points:
+#                    vertices_to_neighboring_points[vertex_idx] = set()
+#                vertices_to_neighboring_points[vertex_idx].add(point_idx)
+#        if -1 not in region:
+#            points_to_neighbors[point_idx] = set()
+#        
+#    for vertex_idx, vertex_points in vertices_to_neighboring_points.items():
+#        for point in vertex_points:
+#            if point in points_to_neighbors:
+#                points_to_neighbors[point].update(vertex_points - set((point,)))
+#                
+#    #print('Neighboring Voronoi points:')
+#    #for point, neighbors in points_to_neighbors.items():
+#    #    print(points[point],':',[points[point] for point in neighbors])
+#    #
+#    #import matplotlib.pyplot as plt
+#    #fig = scipy.spatial.voronoi_plot_2d(voronoi)
+#    #plt.show()
+#    
+#    # compare with handmade function
+#    for region_point_idx, neighbor_point_idcs in points_to_neighbors.items():
+#
+#        # - also curious regarding plot: are there ridges [neighbors] that are not reachable via direct line of sight to point?
+#        # - could be quite useful to have a screenshot of the plot.  it can also be made in a separate window via copy paste of points.
+#
+#        print('Neighboring Voronoi points:')
+#        print(points[region_point_idx],':',[points[point_idx] for point_idx in neighbor_point_idcs])
+#        print('Shifted neighboring voronoi points:')
+#        print(points[region_point_idx],':',[points[point_idx] - points[region_point_idx] for point_idx in neighbor_point_idcs])
+#
+#        handmade_neighbors = set(voronoi_cell(points[region_point_idx], points))
+#
+#        print('Handmade neighboring points:')
+#        print(points[region_point_idx], ':', [*handmade_neighbors])
+#        
+#        import matplotlib.pyplot as plt
+#        fig = scipy.spatial.voronoi_plot_2d(voronoi)
+#        plt.show()
+#
+#        if len(handmade_neighbors) != len(neighbor_point_idcs) + 1:
+#            raise Exception('scipy got', len(neighbor_point_idcs) + 1, 'points, handmade got', len(handmade_neighbors), 'points')
+#        for point_idx in (region_point_idx, *neighbor_point_idcs):
+#            if points[point_idx] not in handmade_neighbors:
+#                raise Exception('handmade not containing point', point_idx)
+#    # todo: compare not coincident with points
+#    return True
+#

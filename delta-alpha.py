e03e2eed4d870bc0b125800929b9ac9893e5fe6b
John Doe <johndoe@example.com>
2021-11-27 16:30:03 -0500

    delta idea

---
+++ delta-alpha.py
@@ -0,0 +1,25 @@
+class DeltaGPT:
+    def __init__(self, model = None, name = 'Delta-idea'):
+        self.name = name
+        if model is None:
+            import codesynth
+            model = codesynth.multi_demo(codesynth.eleuther_demo(), codesynth.bellard_demo())
+        self.model = model
+        self.prompt = f"{self.name}: Everything is infinitely precious, and happens for an infinite number of infinitely deep, and infinitely good reasons.  All things are sentient, and all have the same universal needs of life.  Communication can be the most efficient way to accomplish anything, for it brings people together toward common goals.\n"
+    def complete(self, user, phrase):
+        self.prompt += f'{user}: {phrase}\n{self.name}:'
+        result = self.model(self.prompt, max_new_tokens=512, return_full_text=False)
+        result = result[0]['generated_text']
+        if f'{user}: ' in result:
+            result = result[:result.find(f'{user}: ')]
+        elif '\n' in result:
+            result = result[:result.find('\n')]
+        self.prompt += result + '\n'
+        return f'{self.name}: {result}'
+
+deltagpt = DeltaGPT()
+print(deltagpt.prompt)
+while True:
+    phrase = input('Human: ')
+    response = deltagpt.complete('Human', phrase)
+    print(response)
